9ef450f76e86a5faf2c7e01bfeb026771deda426
herzliyaaa <herzliabarangan@gmail.com>
2021-07-23 03:40:03 +0800

    renamed the title

--- views/admin/users_list.php
+++ views/admin/users_list.php
@@ -7,7 +7,7 @@ include "../../db_connection.php";
 
 <head>
     <meta charset="utf-8" />
-    <title>Barangay Management System</title>
+    <title>Barangay Officials Philippines Record</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" />
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" />
     <!-- <link rel="stylesheet" href="../../public/css/style.css" /> -->
--- views/chairman/dashboard.php
+++ views/chairman/dashboard.php
@@ -3,7 +3,7 @@
 
 <head>
     <meta charset="utf-8" />
-    <title>Punong Barangay Philippines Record</title>
+    <title>Barangay Officials Philippines Record</title>
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" />
