f3c8d53199c50029c962c9f4b65da25dd736d989
zearandal <86549315+zearandal@users.noreply.github.com>
2021-07-22 14:13:14 +0800

    Update dashboard.php

--- views/congressman/dashboard.php
+++ views/congressman/dashboard.php
@@ -9,6 +9,15 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" />
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" />
     <link rel="stylesheet" href="../../public/css/style.css" />
+
+    <style type="text/css">
+        
+        tbody{
+        background: white;
+        color: darkslateblue;
+        }
+
+    </style>
    
 </head>
 
@@ -94,6 +103,14 @@
                                     <th scope="col">Barangay</th>  
                                 </tr>
                             </thead>
+                            <tbody>
+                            <tr><td>1</td>
+                                <td>test</td>
+                                <td>test</td>
+                                <td>test</td>
+                                <td>test</td>
+                            </tr>
+                        </tbody>
                         </table>
                     </div>
                 </div>
