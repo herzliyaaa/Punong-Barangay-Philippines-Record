7154c1530d0cd765436c0dd33a96c6d37864508f
zearandal <loqzloqerbz@gmail.com>
2021-07-26 23:57:54 +0800

    login design changed

--- index.php
+++ index.php
@@ -1,60 +1,69 @@
-<!DOCTYPE html>
-<html lang="en" dir="ltr">
+<!doctype html>
+<html lang="en">
   <head>
-    
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Barangay Officials Philippines Record</title>
-    <link rel="stylesheet" href="public/css/login-style.css" />
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.3.0/font/bootstrap-icons.css" />
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+  <link href="https://fonts.googleapis.com/css?family=Lato:300,400,700,900&display=swap" rel="stylesheet">
+
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+  
+  <link rel="stylesheet" href="public/css/login-style.css">
+
   </head>
-
   <body>
-    <div class="login-section">
-      <div class="logo">
-        <img class="logoimage" src="public/img/phbarangay-logo.png" alt="logo" />
-      </div>
-
-      <div class="text">
-        <h1>PUNONG BARANGAY PHILIPPINES RECORD</h1>
-      </div>
-
-      
- <!-- error -->
- <?php if (isset($_GET['error'])) { ?>
-  <p class="error"><?php echo $_GET['error']; ?></p>
-  <?php } ?>
-
-
-      <form  method="post" class="login" action="login_function.php">
-
-        <div class="textbox">
-          <img src="public/img/icons/user-24.png" alt="" />
-          <input type="text" placeholder="Username"  name="username" required/>
+  <section class="ftco-section">
+    <div class="container">
+      <div class="row justify-content-center">
+        <div class="col-md-12 col-lg-10">
+          <div class="wrap d-md-flex">
+            <div class="text-wrap p-4 p-lg-5 text-center d-flex align-items-center order-md-last">
+              <div  class="text w-100">
+                <div><img src="/login-form-17/images/phbarangay-logo.png"></div>
+                <h2>Welcome to login</h2>
+                <p>Login as Admin to Create User</p>
+              </div>
+            </div>
+            <div class="login-wrap p-4 p-lg-5">
+              <div class="d-flex">
+                <div class="w-100">
+                  <h3 class="mb-4">Barangay Officials Desk</h3>
+                </div>
+              </div>
+              <br>
+              <form method="post" class="login" action="login_function.php">
+                <div class="form-group mb-3">
+                  <i class="fa fa-user icon"></i>
+                  <label class="label" for="name">Username</label>
+                  <input type="text" class="form-control" placeholder="Enter Username" name="username" required>
+                </div>
+                <div class="form-group mb-3">
+                   <i class="fa fa-key icon"></i>
+                  <label class="label" for="password">Password</label>
+                  <input type="password" class="form-control" placeholder="Enter Password" name="password" required> 
+                </div>
+                <div class="form-group">
+                  <button type="submit" class="form-control btn btn-primary submit px-3">CONTINUE</button>
+                </div>
+                <div class="form-group d-md-flex">
+                  <div class="w-50 text-left">
+                  </div>
+                </div>
+              </form>
+            </div>
+          </div>
         </div>
-
-        <div class="textbox">
-          <img src="public/img/icons/password-24.png" alt="" />
-          <input type="password" id= "pass" placeholder="Password" name="password" required/>
-          <i class="bi bi-eye-slash" id="togglePassword"></i>
-        </div>
-
-        <button class="loginbtn">Login</button>
-      </form>
+      </div>
     </div>
+  </section>
+
+  <script src="js/jquery.min.js"></script>
+  <script src="js/popper.js"></script>
+  <script src="js/bootstrap.min.js"></script>
+  <script src="js/main.js"></script>
+
+
   </body>
 </html>
-
-<script type="text/javascript">
-      const togglePassword = document.querySelector('#togglePassword');
-      const pass = document.querySelector('#pass');
-
-      togglePassword.addEventListener('click', function (e) {
-    // toggle the type attribute
-    const type = pass.getAttribute('type') === 'pass' ? 'text' : 'pass';
-    pass.setAttribute('type', type);
-    // toggle the eye / eye slash icon
-    this.classList.toggle('bi-eye');
-});
-
-    </script>
 
