e37616255a39c2f302bead21a3fa4096ae0ac8da
zearandal <loqzloqerbz@gmail.com>
2021-07-27 16:10:48 +0800

    update add official design

--- views/admin/add_officials.php
+++ views/admin/add_officials.php
@@ -1,138 +1,180 @@
 <!DOCTYPE html>
-<html lang="en" dir="ltr">
+<html lang="en">
 
 <head>
+    <!-- Required meta tags-->
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Barangay Officials Philippines Record</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="Colorlib Templates">
+    <meta name="author" content="Colorlib">
+    <meta name="keywords" content="Colorlib Templates">
+
+    <!-- Title Page-->
+    <title>Barangay Officials Philippines Record<</title>
+
+    <!-- Icons font CSS-->
+    <link href="vendor/mdi-font/css/material-design-iconic-font.min.css" rel="stylesheet" media="all">
+    <link href="vendor/font-awesome-4.7/css/font-awesome.min.css" rel="stylesheet" media="all">
+    <!-- Font special for pages-->
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i,800,800i" rel="stylesheet">
+
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" />
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" />
-    <link rel="stylesheet" href="../../public/css/style.css" />
+
+    <!-- Vendor CSS-->
+    <link href="vendor/select2/select2.min.css" rel="stylesheet" media="all">
+    <link href="vendor/datepicker/daterangepicker.css" rel="stylesheet" media="all">
+
+    <!-- Main CSS-->
+    <link href="\Punong-Barangay-Philippines-Record\public\css\add-official.css" rel="stylesheet" media="all">
 
 </head>
 
-
 <body>
 
-    <div class="d-flex" id="wrapper">
+<div class="d-flex" id="wrapper">
         <?php include "../../sidebar.php"; ?>
-        <div id="page-content-wrapper">
+<div id="page-content-wrapper">
 
-            <nav class="navbar navbar-expand-lg navbar-light bg-transparent py-4 px-4">
-                <div class="d-flex align-items-center">
-                    <i class="fas fa-align-left primary-text fs-4 me-3" id="menu-toggle"></i>
-                    <h2 class="fs-3 fw-bold m-0">Add Barangay Officials</h2>
+ <nav class="navbar navbar-expand-lg navbar-light bg-transparent py-4 px-4">
+                <div class="d-flex align-items-center" >
+                    <i class="fas fa-align-left fs-1 me-4" id="menu-toggle" style="margin-top: -2200%;"></i>
                 </div>
 
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
-                        <li class="nav-item dropdown">
-                            <a class="john nav-link dropdown-toggle second-text fw-bold" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                                <i class="doe fas fa-user me-2"></i>John Doe
-                            </a>
-                            <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-
-                                <li><a class="dropdown-item" href="#">Logout</a></li>
-                            </ul>
-                        </li>
-                    </ul>
+    <div class="page-wrapper bg-gra-03 p-t-45 p-b-50">
+        <div class="wrapper wrapper--w790">
+            <div class="card card-5">
+                <div class="card-heading">
+                    <h2 class="title">ADD OFFICIAL</h2>
                 </div>
-            </nav>
-
-            <section>
-                <a href="officials_directory.php">
-                    <button class="back-btn">Back</button>
-                </a>
-
-                <form action="add_officials_function.php" method="post" enctype="multipart/form-data">
-                    <div class="officials-input">
-
-                        <div class="left-input">
-                            <div class="add-input">
-                                <h5>First Name *:</h5>
-                                <input type="text" name="first_name">
-                            </div>
-
-                            <div class="add-input">
-                                <h5>Middle Name *:</h5>
-                                <input type="text" name="middle_name">
-                            </div>
-
-                            <div class="add-input">
-                                <h5>Last Name *:</h5>
-                                <input type="text" name="last_name">
-                            </div>
-
-                            <div class="add-input">
-                                <h5>Suffix :</h5>
-                                <input type="text" name="suffix">
-                            </div>
-
-                            <div class="add-input">
-                                <h5>Position *:</h5>
-                                <input type="text" name="position">
+                <div class="card-body">
+                    <form action="add_officials_function.php" method="post" enctype="multipart/form-data">
+                        <div class="form-row m-b-55">
+                            <div class="name">Name</div>
+                            <div class="value">
+                                <div class="row row-space">
+                                    <div class="col-2">
+                                        <div class="input-group-desc">
+                                            <input class="input--style-5" type="text" name="first_name" required>
+                                            <label class="label--desc">first name</label>
+                                        </div>
+                                    </div>
+                                     <div class="col-2">
+                                        <div class="input-group-desc">
+                                            <input class="input--style-5" type="text" name="middle_name">
+                                            <label class="label--desc">Middle name</label>
+                                        </div>
+                                    </div>
+                                    <div style="margin-top: 10%;" class="col-2">
+                                        <div class="input-group-desc">
+                                            <input class="input--style-5" type="text" name="last_name" required>
+                                            <label class="label--desc">last name</label>
+                                        </div>
+                                    </div>
+                                      <div style="margin-top: 10%;" class="col-2">
+                                        <div class="input-group-desc">
+                                            <input class="input--style-5" type="text" name="suffix">
+                                            <label class="label--desc">Suffix</label>
+                                        </div>
+                                    </div>
+                                </div>
                             </div>
                         </div>
-
-                        <div class="right-input">
-                            <div class="add-input">
-                                <h5>Region *:</h5>
-                                <input type="text" name="region">
-                            </div>
-
-
-
-
-                            <div class="add-input">
-                                <h5>Province *:</h5>
-                                <input type="text" name="province">
-                            </div>
-
-
-                            <div class="add-input">
-                                <h5>City/Municipality *:</h5>
-                                <input type="text" name="city">
-                            </div>
-
-                            <div class="add-input">
-                                <h5>Barangay *:</h5>
-                                <input type="text" name="barangay">
-                            </div>
-
-                            <div class="add-input">
-                                <h5>Email Address *:</h5>
-                                <input type="text" name="email">
+                           <div class="form-row">
+                            <div class="name">Position</div>
+                            <div class="value">
+                                <div class="input-group">
+                                    <div class="rs-select2 js-select-simple select--no-search">
+                                        <select name="position" required>
+                                            <option disabled="disabled" selected="selected">Choose option</option>
+                                            <option>SK Chairperson</option>
+                                            <option>Punong Barangay</option>
+                                            <option>Sangguniang Barangay Member</option>
+                                        </select>
+                                        <div class="select-dropdown"></div>
+                                    </div>
+                                </div>
                             </div>
                         </div>
-                        <div class="outer-right-input">
-                            <div class="add-input">
-                                <h5>Barangay Hall Phone Number *:</h5>
-                                <input type="text" name="barangay_hall_tel_no">
+                        <div class="form-row">
+                            <div class="name">Region</div>
+                            <div class="value">
+                                <div class="input-group">
+                                    <input class="input--style-5" type="text" name="region" required>
+                                </div>
                             </div>
-
-
-                            <button class="save-btn" name="add_official_btn">Save</button>
                         </div>
-                    </div>
+                          <div class="form-row">
+                            <div class="name">Province</div>
+                            <div class="value">
+                                <div class="input-group">
+                                    <input class="input--style-5" type="text" name="province" required>
+                                </div>
+                            </div>
+                        </div>
+                          <div class="form-row">
+                            <div class="name">City</div>
+                            <div class="value">
+                                <div class="input-group">
+                                    <input class="input--style-5" type="text" name="city" required>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="form-row">
+                            <div class="name">Barangay</div>
+                            <div class="value">
+                                <div class="input-group">
+                                    <input class="input--style-5" type="text" name="barangay" required>
+                                </div>
+                            </div>
+                        </div>
+                         <div class="form-row">
+                            <div class="name">Email</div>
+                            <div class="value">
+                                <div class="input-group">
+                                    <input class="input--style-5" type="email" name="email">
+                                </div>
+                            </div>
+                        </div>
+                        <div class="form-row m-b-55">
+                            <div class="name">Telephone</div>
+                            <div class="value">
+                                <div class="row row-refine">
+                                    <div class="col-9">
+                                        <div class="input-group-desc">
+                                            <input class="input--style-5" type="text" name="barangay_hall_tel_no">
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="form-row p-t-20">
+                            <label class="label label--block">By clicking the Register button below, I hereby agree to and accept the following terms and conditions governing my
+use of the website.</label>
+                        </div>
+                        <div>
+                            <button class="btn btn--radius-2 btn--red" type="submit" name="add_official_btn">Register</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
         </div>
-
-        </form>
-
-        </section>
-
-
-
     </div>
-    </div>
-
-</body>
 </div>
+    <!-- Jquery JS-->
+    <script src="vendor/jquery/jquery.min.js"></script>
+    <!-- Vendor JS-->
+    <script src="vendor/select2/select2.min.js"></script>
+    <script src="vendor/datepicker/moment.min.js"></script>
+    <script src="vendor/datepicker/daterangepicker.js"></script>
+
+    <!-- Main JS-->
+    <script src="js/global.js"></script>
+
+</body><!-- This templates was made by Colorlib (https://colorlib.com) -->
 
 </html>
+
 
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js"></script>
 
@@ -144,3 +186,4 @@
         el.classList.toggle("toggled");
     };
 </script>
+<!-- end document-->
